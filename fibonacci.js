d4546d817aa43eb24499a140ec7c772c3d71e394
Arielle M <69764349+DoctorStupidMD@users.noreply.github.com>
2020-10-17 20:41:32 -0300

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -4,21 +4,6 @@
 // 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
 
 
-// Initial thoughts:
-  // (2 - 2) + (2 - 1) = 1
-  // Index 0 value + index 1 value 
-
-  // (3 - 2) + (3 - 1) = 3
-  // Index 1 value + index 2 value
-
-  // (4 - 2) + (4 - 1) = 5
-  // Index 2 value + index 3 value 
-
-  // (5 - 2) + (5 - 1) = 7
-  // Index 3 value + index 4 value
-
-
-
 // Case #1: Iterative.
   // To call the function, pass in the nth index. 
   // The function returns the integer corresponding to that index in the sequence. 
@@ -33,7 +18,6 @@ function fib1(n) {
 
   return array[n];
 }
-
 
 
 // Case #2: Recursive. One based.
